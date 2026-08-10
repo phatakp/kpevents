cb403cfce6d347c04fa440b5ed152d723b9a39c4
Praveen Phatak <praveenphatak@gmail.com>
2026-08-10 04:26:21 +0000

    fix nitro build

--- .vercel/output/functions/__server.func/_libs/motion.mjs
+++
@@ -1,5 +0,0 @@
-import { motion as motion$1 } from "./framer-motion.mjs";
-//#region node_modules/motion/dist/es/react.mjs
-var motion = motion$1;
-//#endregion
-export { motion };
--- .vercel/output/functions/__server.func/_ssr/utils-D_6BNord.mjs
+++
@@ -1,5 +0,0 @@
-import { buildErrorThrower } from "../_libs/clerk__shared.mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/utils-D_6BNord.js
-var errorThrower = buildErrorThrower({ packageName: "@clerk/tanstack-react-start" });
-//#endregion
-export { errorThrower };
